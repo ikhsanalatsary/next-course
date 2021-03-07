9200f18beaef759369dbd670b5864b637ac43073
ikhsanalatsary <skull.saders18@gmail.com>
2021-03-08 00:51:28 +0700

    add & configure material ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,35 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import React from "react";
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "../lib/apolloClient";
+import theme from "../lib/theme";
 
 function MyApp({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState);
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
   return (
     <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </ApolloProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
