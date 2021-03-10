02bf303bebce529b783b08970fd732ce2ba873d2
ikhsanalatsary <skull.saders18@gmail.com>
2021-03-10 08:57:49 +0700

    fix apollo cannot load env file right away

--- apollo.config.js
+++ apollo.config.js
@@ -1,8 +1,15 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
+if (!process.env.NEXT_PUBLIC_GRAPHQL_URI) {
+  require('dotenv').config({
+    path: '.env.local',
+  });
+}
 module.exports = {
   client: {
-    includes: ["pages/**", "components/**"],
+    includes: ['pages/**', 'components/**'],
     service: {
-      name: "next-course",
+      name: 'next-course',
       url: process.env.NEXT_PUBLIC_GRAPHQL_URI,
     },
   },
