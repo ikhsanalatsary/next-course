43da872587fbf93faa331b6c166bddd3a5279349
ikhsanalatsary <skull.saders18@gmail.com>
2021-03-19 08:31:12 +0700

    use local schema, boost startup

--- apollo.config.js
+++ apollo.config.js
@@ -1,16 +1,9 @@
-/* eslint-disable global-require */
-/* eslint-disable import/no-extraneous-dependencies */
-if (!process.env.NEXT_PUBLIC_GRAPHQL_URI) {
-  require('dotenv').config({
-    path: '.env.local',
-  });
-}
 module.exports = {
   client: {
     includes: ['pages/**', 'components/**'],
     service: {
       name: 'next-course',
-      url: process.env.NEXT_PUBLIC_GRAPHQL_URI,
+      localSchemaFile: './graphql.schema.json',
     },
   },
 };
